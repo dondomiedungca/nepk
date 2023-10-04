e408fc6fc303d1d1e4d4f4dd088eef6d77f3ec2a
Dondomie Dungca <41827126+nodnod08@users.noreply.github.com>
2023-10-04 19:33:38 +0800

    Change my mind

--- packages/config/src/config.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from './config.service';
-
-@Module({
-  imports: [],
-  providers: [ConfigService],
-})
-export class ConfigModule {}
